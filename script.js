df23e4343a67689499622e2a8e5f841e62126942
AlgodaodaJu <lkaua9428@gmail.com>
2025-07-19 12:10:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function switchLang(lang) {
-  document.body.lang = lang;
+  document.documentElement.lang = lang;
   document.querySelectorAll('[data-lang]').forEach(el => {
     el.style.display = (el.getAttribute('data-lang') === lang) ? '' : 'none';
   });
